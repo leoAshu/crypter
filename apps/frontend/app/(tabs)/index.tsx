427ec94bfeb9de761797bcf684cf7ec884ea7c2e
leoAshu <ashutosh.ojha2009@gmail.com>
2025-07-28 17:42:46 -0400

    fixes initials avatar size preset

--- apps/frontend/app/(tabs)/index.tsx
+++ apps/frontend/app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
+import { InitialsAvatar } from '@/components';
+import { useAuthStore } from '@/store';
 import { ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
+  const { user } = useAuthStore();
   return (
     <SafeAreaView className='screen-wrapper'>
       <ScrollView>
-        <View className='content-wrapper mt-20'>
-          <Text className='text-black dark:text-white'>Home</Text>
+        <View className='content-wrapper mt-6'>
+          <InitialsAvatar name={user?.user_metadata.name} size='small' />
+          <Text className='header-txt font-poppins-medium'>Hello,</Text>
+          <Text className='header-txt -mt-6 font-poppins-medium'>{user?.user_metadata.name} 👋</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
