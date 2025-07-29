1f65015b9e3f468cec8b60afd30104190afbbef0
leoAshu <ashutosh.ojha2009@gmail.com>
2025-07-29 00:02:52 -0400

    adds haptics to fab press

--- apps/frontend/components/tab/FloatingActionTabButton.tsx
+++ apps/frontend/components/tab/FloatingActionTabButton.tsx
@@ -1,3 +1,4 @@
+import * as Haptics from 'expo-haptics';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Image, Platform, Pressable, useColorScheme, View } from 'react-native';
 import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
@@ -11,6 +12,7 @@ const FloatingActionTabButton = (props: FloatingActionTabButtonProps) => {
   const innerShadowOpacity = useSharedValue(1);
 
   const handlePressIn = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
     scale.value = withTiming(isIOS ? 0.94 : 1, { duration: 100 });
     outerShadowOpacity.value = withTiming(1, { duration: 120 });
     innerShadowOpacity.value = withTiming(0, { duration: 120 });
