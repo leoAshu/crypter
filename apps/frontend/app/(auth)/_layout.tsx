b6dffa2dd4477b1496634f2996221c0736d6c565
mano <manoranjan.kumar.dev@gmail.com>
2025-08-03 01:18:36 +0530

    Enhances AuthLayout with dynamic header styling and HeaderBackAction for SignUpInfo

--- apps/frontend/app/(auth)/_layout.tsx
+++ apps/frontend/app/(auth)/_layout.tsx
@@ -1,16 +1,31 @@
+import { HeaderBackAction } from '@/components';
 import { Stack } from 'expo-router';
+import { Platform, useColorScheme } from 'react-native';
 
 const AuthLayout = () => {
+  const isDark = useColorScheme() === 'dark';
+  const containerStyle = Platform.select({
+    ios: 'pl-0 ml-[8px]',
+    android: 'pl-0 ml-[8px]',
+  });
   return (
     <Stack
       screenOptions={() => ({
-        headerShown: false,
+        headerTransparent: true,
+        headerStyle: {
+          backgroundColor: isDark ? '#000000' : '#FFFFFF',
+        },
       })}
     >
       <Stack.Screen name='signin' />
       <Stack.Screen name='welcome' />
       <Stack.Screen name='(signup)/index' />
-      <Stack.Screen name='(signup)/info' />
+      <Stack.Screen
+        name='(signup)/info'
+        options={{
+          headerLeft: () => <HeaderBackAction containerStyle={containerStyle} />,
+        }}
+      />
     </Stack>
   );
 };
