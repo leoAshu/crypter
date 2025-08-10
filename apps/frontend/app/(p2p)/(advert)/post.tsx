78a27f023f5d39b6547b45887802b6e5b29352ec
mano <manoranjan.kumar.dev@gmail.com>
2025-08-10 16:46:04 +0530

    updates the post advert page to include new components

--- apps/frontend/app/(p2p)/(advert)/post.tsx
+++ apps/frontend/app/(p2p)/(advert)/post.tsx
@@ -1,17 +1,29 @@
 import { Dropdown, ToggleButton } from '@/components';
-import { screenContentWrapperStyle } from '@/constants';
+import { PriceSelector } from '@/components/selectors';
+import { screenContentWrapperStyle, Strings } from '@/constants';
 import { useAds, useCrypto } from '@/hooks';
+import { PRICE_SELECTOR_RANGE, PriceType } from '@/models';
+import { capitalizeWords } from '@/utils';
 import cn from 'clsx';
 import { useState } from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const PostAdvert = () => {
-  const { adTypeFilterItems } = useAds();
-  const { cryptoNameFilterItemsStrict, getCryptoNameFilterItemById } = useCrypto();
+  const PRICE_INDEX: Record<PriceType, number> = {
+    [PriceType.Fixed]: 0,
+    [PriceType.Floating]: 0,
+  } as const;
 
+  const { adTypeFilterItems } = useAds();
+  const { priceTypeFilterItems } = useAds();
+  const { cryptoNameFilterItemsStrict, getCryptoNameFilterItemById } = useCrypto();
   const [adType, setAdType] = useState<FilterItem>(adTypeFilterItems[0]);
+  const [priceType, setpriceType] = useState<FilterItem>(priceTypeFilterItems[0]);
   const [selectedCrypto, setSelectedCrypto] = useState<FilterItem>();
+  const [priceIndices, setPriceIndices] = useState<Record<string, number>>(PRICE_INDEX);
+
+  const currentIndex = priceIndices[priceType.id];
 
   const handleCreateAdvert = async () => {
     if (!selectedCrypto) {
@@ -22,6 +34,7 @@ const PostAdvert = () => {
     const advertData = {
       type: adType,
       cryptoId: selectedCrypto.id,
+      priceType: priceType,
       // we will add more fields here later
     };
 
@@ -57,6 +70,38 @@ const PostAdvert = () => {
           items={cryptoNameFilterItemsStrict}
           value={selectedCrypto}
           onSelect={(crypto) => setSelectedCrypto(getCryptoNameFilterItemById(crypto.id))}
+        />
+        <Text className='text-label'>{Strings.postAd.PRICE_SETTING}</Text>
+        <ToggleButton
+          value={priceType}
+          items={[priceTypeFilterItems[0], priceTypeFilterItems[1]]}
+          activeButtonColors={{
+            [priceTypeFilterItems[0].id]: 'bg-card-info',
+            [priceTypeFilterItems[1].id]: 'bg-card-info',
+          }}
+          activeLabelColors={{
+            [priceTypeFilterItems[0].id]: 'text-base-dark',
+            [priceTypeFilterItems[1].id]: 'text-base-dark',
+          }}
+          wrapperStyle='w-full h-10'
+          onChange={(val) => setpriceType(val)}
+        />
+        <PriceSelector
+          label={capitalizeWords(priceType.label)}
+          onIncrement={() => {
+            setPriceIndices((prev) => ({
+              ...prev,
+              [priceType.id]: prev[priceType.id] + 1,
+            }));
+          }}
+          onDecrement={() => {
+            setPriceIndices((prev) => ({
+              ...prev,
+              [priceType.id]: prev[priceType.id] - 1,
+            }));
+          }}
+          index={currentIndex}
+          items={PRICE_SELECTOR_RANGE[priceType.id]}
         />
       </View>
     </SafeAreaView>
