26af6e3fd8087a643f3ff54bacf594d343095f0e
mano <manoranjan.kumar.dev@gmail.com>
2025-08-12 00:21:06 +0530

    Renames priceTypes to priceType and removes the priceTypes model file

--- apps/frontend/models/index.ts
+++ apps/frontend/models/index.ts
@@ -1,7 +1,7 @@
 export * from './ad';
 export * from './order';
 export * from './payMethods';
-export * from './priceTypes';
+export * from './priceType';
 export * from './review';
 export * from './stats';
 
