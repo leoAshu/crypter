e139821750d784711e15b5e442c5da8d98059a33
leoAshu <ashutosh.ojha2009@gmail.com>
2025-08-13 11:08:32 -0400

    prettier format

--- apps/frontend/components/toasts/InfoToast.tsx
+++ apps/frontend/components/toasts/InfoToast.tsx
@@ -6,12 +6,12 @@ const InfoToast = (props: BaseToastProps) => {
   const isDark = useColorScheme() === 'dark';
 
   return (
-    <View className='border-b-info w-[86%] gap-y-2 rounded border-b-2 bg-card px-6 pb-6 pt-4 shadow-md dark:bg-card-dark'>
+    <View className='w-[86%] gap-y-2 rounded border-b-2 border-b-info bg-card px-6 pb-6 pt-4 shadow-md dark:bg-card-dark'>
       <View className='flex-row items-center gap-x-3'>
         <Image source={isDark ? icons.dark.infoCircle : icons.light.infoCircle} className='size-6' />
 
         <View className='flex-1 flex-row items-center justify-between'>
-          {props.text1 && <Text className='text-info-500 font-manrope-bold text-sm'>{props.text1}</Text>}
+          {props.text1 && <Text className='font-manrope-bold text-sm text-info-500'>{props.text1}</Text>}
           <Pressable onPress={() => Toast.hide()} hitSlop={20}>
             <Image source={images.x} className='size-5' tintColor={isDark ? '#667085' : '#98A2B3'} />
           </Pressable>
